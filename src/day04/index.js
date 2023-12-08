79b77d0a75846f3394d61816a411c419e6c1209e
stuwilmur <stuwilmur@users.noreply.github.com>
2023-12-08 18:06:47 +0000

    Solve day 4 part 2

--- src/day04/index.js
+++ src/day04/index.js
@@ -1,5 +1,5 @@
 import run from "aocrunner";
-import { count } from "console";
+import { count, table } from "console";
 
 const parseInput = (rawInput) => rawInput.split("\n");
 
@@ -21,7 +21,12 @@ function parseLine(line) {
   const have = new Set(parseNumbers(matches[3]));
   const winners = intersection(winning, have).size;
   const points = winners > 0 ? 2 ** (winners - 1) : 0;
-  return { cardNumber: cardNumber, points: points };
+  return {
+    cardNumber: cardNumber,
+    points: points,
+    winners: winners,
+    copies: 1,
+  };
 }
 
 const countPoints = (results) =>
@@ -29,9 +34,35 @@ const countPoints = (results) =>
 
 const part2 = (rawInput) => {
   const input = parseInput(rawInput);
-
-  return;
+  let table = input.map(parseLine);
+  const cardNumbers = table.map((x) => x.cardNumber);
+  cardNumbers.forEach((cardNumber) => {
+    table = processCard(cardNumber, table);
+  });
+  //console.log(table);
+  //console.log(countCopies(table));
+  return countCopies(table);
 };
+
+const countCopies = (list) => list.map((x) => x.copies).reduce((a, b) => a + b);
+
+const getCard = (table, id) => table.find((x) => x.cardNumber == id);
+
+function processCard(thisCardNumber, table) {
+  let dup = table.slice();
+  const thisCard = getCard(dup, thisCardNumber);
+  for (
+    let cardNumber = thisCardNumber + 1;
+    cardNumber < thisCardNumber + thisCard.winners + 1;
+    cardNumber++
+  ) {
+    const card = getCard(dup, cardNumber);
+    if (card != undefined) {
+      card.copies += thisCard.copies;
+    }
+  }
+  return dup;
+}
 
 run({
   part1: {
@@ -51,10 +82,16 @@ Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11`,
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `
+Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11`,
+        expected: 30,
+      },
     ],
     solution: part2,
   },
