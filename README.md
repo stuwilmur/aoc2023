06d11d1f5a25dedfdae631508445983ba48b1cc3
stuwilmur <stuwilmur@users.noreply.github.com>
2023-12-11 22:58:52 +0000

    Solve day 9, part 1

--- README.md
+++ README.md
@@ -19,7 +19,7 @@
 [![Day](https://badgen.net/badge/06/%E2%98%85%E2%98%85/green)](src/day06)
 [![Day](https://badgen.net/badge/07/%E2%98%85%E2%98%85/green)](src/day07)
 [![Day](https://badgen.net/badge/08/%E2%98%85%E2%98%85/green)](src/day08)
-![Day](https://badgen.net/badge/09/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/09/%E2%98%85%E2%98%86/yellow)](src/day09)
 ![Day](https://badgen.net/badge/10/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/11/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/12/%E2%98%86%E2%98%86/gray)
@@ -125,9 +125,9 @@ Both parts: 24.367ms
 
 ```
 Day 09
-Time part 1: -
+Time part 1: 10.068ms
 Time part 2: -
-Both parts: -
+Both parts: 10.068ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 8/50
-Total time: 15556.789ms
+Total stars: 9/50
+Total time: 15566.858ms
 ```
 
 <!--/RESULTS-->
---
+++ src/day09/index.js
@@ -0,0 +1,59 @@
+import run from "aocrunner";
+
+const parseInput = (rawInput) =>
+  rawInput
+    .split("\n")
+    .map((line) => line.match(/-?\d+/g).map((d) => parseInt(d)));
+
+function extrap(seq) {
+  const set = new Set(seq);
+  if (set.size == 1) {
+    return seq.concat(seq[0]);
+  }
+  return seq.concat(seq.slice(-1)[0] + extrap(diff(seq)).slice(-1)[0]);
+}
+
+function diff(seq) {
+  let diffs = [];
+  for (let i = 0; i < seq.length - 1; i++) {
+    diffs.push(seq[i + 1] - seq[i]);
+  }
+  return diffs;
+}
+
+const part1 = (rawInput) => {
+  const input = parseInput(rawInput);
+  const extrapolated = input.map((d) => extrap(d).slice(-1)[0]);
+  return extrapolated.reduce((a, b) => a + b);
+};
+
+const part2 = (rawInput) => {
+  const input = parseInput(rawInput);
+
+  return;
+};
+
+run({
+  part1: {
+    tests: [
+      {
+        input: `0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45`,
+        expected: 114,
+      },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      // {
+      //   input: ``,
+      //   expected: "",
+      // },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+  onlyTests: false,
+});
