ec1252e1e54ed2d3e424bd6914db9c7dc0818ebe
stuwilmur <stuwilmur@users.noreply.github.com>
2023-12-11 23:23:51 +0000

    Solve day 9, part 2

--- README.md
+++ README.md
@@ -19,7 +19,7 @@
 [![Day](https://badgen.net/badge/06/%E2%98%85%E2%98%85/green)](src/day06)
 [![Day](https://badgen.net/badge/07/%E2%98%85%E2%98%85/green)](src/day07)
 [![Day](https://badgen.net/badge/08/%E2%98%85%E2%98%85/green)](src/day08)
-[![Day](https://badgen.net/badge/09/%E2%98%85%E2%98%86/yellow)](src/day09)
+[![Day](https://badgen.net/badge/09/%E2%98%85%E2%98%85/green)](src/day09)
 ![Day](https://badgen.net/badge/10/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/11/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/12/%E2%98%86%E2%98%86/gray)
@@ -125,9 +125,9 @@ Both parts: 24.367ms
 
 ```
 Day 09
-Time part 1: 10.068ms
-Time part 2: -
-Both parts: 10.068ms
+Time part 1: 10.813ms
+Time part 2: 7.803ms
+Both parts: 18.616ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 9/50
-Total time: 15566.858ms
+Total stars: 10/50
+Total time: 15575.406ms
 ```
 
 <!--/RESULTS-->
--- src/day09/index.js
+++ src/day09/index.js
@@ -21,16 +21,27 @@ function diff(seq) {
   return diffs;
 }
 
-const part1 = (rawInput) => {
-  const input = parseInput(rawInput);
+const solve = (input) => {
   const extrapolated = input.map((d) => extrap(d).slice(-1)[0]);
   return extrapolated.reduce((a, b) => a + b);
 };
 
-const part2 = (rawInput) => {
+const part1 = (rawInput) => {
   const input = parseInput(rawInput);
+  return solve(input);
+};
 
-  return;
+const parseInput2 = (rawInput) =>
+  rawInput.split("\n").map((line) =>
+    line
+      .match(/-?\d+/g)
+      .reverse()
+      .map((d) => parseInt(d)),
+  );
+
+const part2 = (rawInput) => {
+  const input = parseInput2(rawInput);
+  return solve(input);
 };
 
 run({
@@ -47,10 +58,12 @@ run({
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45`,
+        expected: 2,
+      },
     ],
     solution: part2,
   },
